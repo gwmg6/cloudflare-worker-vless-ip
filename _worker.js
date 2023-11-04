b60318d515f5c13a3ab39d1827cf6c460ffeee1d
Gwming <139041480+gwmg6@users.noreply.github.com>
2023-11-04 23:26:12 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '72c57c41-9cd9-46d0-840f-b81bc4c88934';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org';
+let proxyIP = 'www.glassdoor.com';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
